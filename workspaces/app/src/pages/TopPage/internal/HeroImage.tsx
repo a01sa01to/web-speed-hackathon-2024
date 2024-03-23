1d4670c472094f246df58567419c1bd2b78c6eec
Asa <26807394+a01sa01to@users.noreply.github.com>
2024-03-23 16:16:18 +0900

    Hero の Aspect ratio おかしい

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 import { IMAGE_SRC } from './ImageSrc';
 
 const _Wrapper = styled.div`
-  aspect-ratio: 16 / 9;
   width: 100%;
 `;
 
 const _Image = styled.img`
   display: inline-block;
   width: 100%;
+  aspect-ratio: 16 / 9;
 `;
 
 export const HeroImage: React.FC = () => {
